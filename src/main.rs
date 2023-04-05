33691d21d7e1a9ac0edf1ed42d4f64fffd23b35d
strongchris <chris@strongcompute.com>
2023-04-06 09:41:11 +1000

    a step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use native_tls::{TlsConnector, TlsStream};
+
+use mailparse::*;
+
 fn main() {
     println!("Hello, world!");
+    let tls = TlsConnector::builder().build().unwrap();
+    
+    let client = imap::connect(
+        ("imap.gmail.com", 993),
+        "imap.gmail.com",
+        &tls,
+    ).unwrap();
+
+    let username = std::env::var("USERNAME").unwrap();
+    let password = std::env::var("PASSWORD").unwrap();
+
+    let mut imap_session = client.login(username, password).unwrap();
+
+    imap_session.select("[Gmail]/All Mail").unwrap();
 }
+
